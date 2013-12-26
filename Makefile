cb22c16620303ea3bfbe1ff9bf62e6a1a6161d24
Casey Scarborough <caseyscarborough@gmail.com>
2013-12-26 17:59:27 -0500

    Add exercise 5.

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ CFLAGS=-Wall -g
 all:
 	cc ex1.c -o ex1
 	cc ex3.c -o ex3
+	cc ex5.c -o ex5
 
 clean:
-	rm ex1 ex3
+	rm ex1 ex3 ex5
---
+++ ex5.c
@@ -0,0 +1,18 @@
+/**
+ * Learn C The Hard Way
+ *
+ * Exercise 5: The Structure Of A C Program
+ */
+
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char * argv[])
+{
+  int distance = 100;
+  
+  // This is also a comment.
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
